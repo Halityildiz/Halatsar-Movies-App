c273185336a28c73625e09ce2a0146af74a0597a
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-25 01:01:21 +0300

    v2

--- src/auth/firebase-config.js
+++ src/auth/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD_mwwRKpe_H-AGlEDwf2ZDY9kTbNJ5m8w",
-  authDomain: "moviestar-app-e1f20.firebaseapp.com",
-  projectId: "moviestar-app-e1f20",
-  storageBucket: "moviestar-app-e1f20.appspot.com",
-  messagingSenderId: "842286896568",
-  appId: "1:842286896568:web:8afc86f15ba88878a2ab4d",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
